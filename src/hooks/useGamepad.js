314d26865cee128e57ee1ce9fffdbb974d3ca1ce
Isa Josep <isaurini1902@gmail.com>
2025-01-09 21:46:32 -0600

    mapped buttons

--- src/hooks/useGamepad.js
+++ src/hooks/useGamepad.js
@@ -1,12 +1,18 @@
 import { useEffect, useRef } from "react";
 
 const BUTTON_MAP = {
-  0: { action: "ADD_JUDGE_ACTION", color: "blueScore", points: 1 }, // Botón A
-  1: { action: "ADD_JUDGE_ACTION", color: "redScore", points: 1 },  // Botón B
+  0: { action: "ADD_JUDGE_ACTION", color: "blueScore", points: 1 }, // Botón A: 1 punto azul
+  1: { action: "ADD_JUDGE_ACTION", color: "blueScore", points: 2 }, // Botón B: 2 puntos azul
+  2: { action: "ADD_JUDGE_ACTION", color: "redScore", points: 1 },  // Botón X: 1 punto rojo
+  3: { action: "ADD_JUDGE_ACTION", color: "redScore", points: 2 },  // Botón Y: 2 puntos rojo
+  4: { action: "ADD_JUDGE_ACTION", color: "redScore", points: 5 }, // Botón LB: 5 puntos rojo
+  5: { action: "ADD_JUDGE_ACTION", color: "blueScore", points: 5 }, // Botón RB: 5 puntos azul
+  6: { action: "ADD_JUDGE_ACTION", color: "redScore", points: 3 },  // Botón LT: 3 puntos rojo
+  7: { action: "ADD_JUDGE_ACTION", color: "blueScore", points: 3 }, // Botón RT: 3 puntos azul
 };
 
 export const useGamepad = (onButtonPress) => {
-  const buttonState = useRef({}); // Almacena el estado de los botones
+  const buttonState = useRef({}); 
 
   useEffect(() => {
     const handleGamepadInput = () => {
